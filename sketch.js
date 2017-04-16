0bd1cc6403eb643c03e5fe8a10109368006d5d37
nikitabala19 <nikitabala19@gmail.com>
2017-04-16 16:18:39 -0400

    Adding info beside the canvas

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 var nodes = [];
 //var centroids = [];
-var buttonNext,buttonStart,buttonGo,buttonFile ;
+var buttonNext,buttonStart,buttonGo,buttonFile,p,infoSpan,center;
 var clusters = [];
 var h1;
 var seeds;
@@ -27,9 +27,17 @@ function setup() {
 	var fileSelect = createFileInput(fileUploaded, 'multiple');
 	fileSelect.id('upfile');
 	fileSelect.parent('upfilediv');
-    //createCanvas(600,400);
-    var can=createCanvas(960, 422.4);
+	var maindiv=createDiv("");
+	maindiv.id('maindiv');
+	var canspan=createDiv("");
+	canspan.id('canspan');
+	infoSpan=createDiv("");
+	infoSpan.id('infoSpan');
+	canspan.parent('maindiv');
+	infoSpan.parent('maindiv');
+    var can=createCanvas(800, 422.4);
 	can.class('canvasStyle');
+	can.parent('canspan');
     createP("");
 
     // Create seeds for clustering randomly.
